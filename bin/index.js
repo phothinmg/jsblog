4dedf8787950bfb51aae4ab36d508a160b76f7c3
phothinmg <phothinmg@gmail.com>
2024-01-21 04:40:49 +0630

    modified:   bin/index.js

--- bin/index.js
+++ bin/index.js
@@ -1,48 +1,46 @@
 #!/usr/bin/env node
 
-import {execSync} from 'child_process';
-import { program } from 'commander';
-import pkg from "../package.json";
-const runCommand = command => {
-    try{
-        execSync(`${command}`, {stdio: 'inherit'});
-    } catch(e){
-        console.error(`Fail to excute ${command}`, e);
-        return false;
-    }
-    return true;
-}
-const devCommand = 'node ./node_modules/jsblog/src/opt/dev/index.js';
-const buildCommand = 'npx cross-env NODE_ENV=production node ./node_modules/jsblog/src/opt/build/index.js';
-const startCommand = 'node ./node_modules/jsblog/src/opt/start/index.js';
+import { execSync } from "child_process";
+import { program } from "commander";
+import pkg from "../package.json" assert { type: "json" };
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (e) {
+    console.error(`Fail to excute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+const devCommand = "node ./node_modules/jsblog/src/opt/dev/index.js";
+const buildCommand = "npx cross-env NODE_ENV=production node ./node_modules/jsblog/src/opt/build/index.js";
+const startCommand = "node ./node_modules/jsblog/src/opt/start/index.js";
 program.version = pkg.version;
 
 program
-    .command('dev')
-    .description('Development process of app.')
-    .action(()=>{
-        const checkOut = runCommand(devCommand);
-        if(!checkOut) process.exit(-1);
-        console.log('Done !!!');
-    });
+  .command("dev")
+  .description("Development process of app.")
+  .action(() => {
+    const checkOut = runCommand(devCommand);
+    if (!checkOut) process.exit(-1);
+    console.log("Done !!!");
+  });
 
 program
-    .command('build')
-    .description('Build process of app.')
-    .action(()=>{
-        const checkOut = runCommand(buildCommand);
-        if(!checkOut) process.exit(-1);
-        console.log('Done !!!');
-    });
+  .command("build")
+  .description("Build process of app.")
+  .action(() => {
+    const checkOut = runCommand(buildCommand);
+    if (!checkOut) process.exit(-1);
+    console.log("Done !!!");
+  });
 program
-    .command('start')
-    .description('Start the app.')
-    .action(()=>{
-        const checkOut = runCommand(startCommand);
-        if(!checkOut) process.exit(-1);
-        console.log('Done !!!');
-    });
+  .command("start")
+  .description("Start the app.")
+  .action(() => {
+    const checkOut = runCommand(startCommand);
+    if (!checkOut) process.exit(-1);
+    console.log("Done !!!");
+  });
 
 program.parse(process.argv);
-
-
