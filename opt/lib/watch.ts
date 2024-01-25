4cabd8f6df43bc1bcac9bdf21c5adcdaeedf4de2
phothinmg <phothinmg@gmail.com>
2024-01-25 12:09:04 -0500

    modified:   opt/lib/watch.ts

--- opt/lib/watch.ts
+++ opt/lib/watch.ts
@@ -1,23 +1,21 @@
-import chokidar from 'chokidar';
-import chalk from 'chalk';
-import { generator } from './generate.js';
-import rebuild from '../rebuild/index.js';
+import chokidar from "chokidar";
+import chalk from "chalk";
+import { generator } from "./generate.js";
 
-
-export default function watcher(outDir: string,appDir: string){
-    console.log(chalk.bgBlack.yellow.bold('MINGALARPAR  🙏🙏🙏 .....'));
-    const watch = chokidar.watch(appDir, {
-        ignoreInitial: true,
+export default function watcher(outDir: string, appDir: string) {
+  console.log(chalk.bgBlack.yellow.bold("MINGALARPAR  🙏🙏🙏 ....."));
+  const watch = chokidar.watch(appDir, {
+    ignoreInitial: true,
+  });
+  watch
+    .on("all", (event, changePath) => {
+      console.log(chalk.yellow(`${changePath} has been changed.Reloading ...`));
+      generator(outDir, appDir);
+    })
+    .on("error", (error) => {
+      console.error(chalk.magenta(`Watcher error: ${error}`));
+    })
+    .on("ready", () => {
+      console.log(chalk.bgBlack.green("Ready to watch"));
     });
-    watch
-     .on('all', (event, changePath) => {
-        console.log (chalk.yellow(`${changePath} has been changed.Reloading ...`));
-        rebuild(outDir,appDir);
-     })
-     .on('error', (error) => {
-        console.error(chalk.magenta(`Watcher error: ${error}`));
-      })
-      .on('ready', () => {
-        console.log(chalk.bgBlack.green('Ready to watch'));
-      });
 }
